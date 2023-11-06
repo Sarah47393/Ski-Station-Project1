8d2038665f31fa34dcfa71630a9d5e9b657df30d
gmesseng <stage_data_science@kaisensdata.fr>
2023-11-06 23:53:21 +0100

    update classes test

---
+++ src/test/java/tn/esprit/Service/CourseServicesImplMock.java
@@ -0,0 +1,52 @@
+package tn.esprit.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.junit.jupiter.api.Assertions;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.SkiStationProject.entities.Course;
+import tn.esprit.SkiStationProject.entities.enums.Support;
+import tn.esprit.SkiStationProject.entities.enums.TypeCourse;
+import tn.esprit.SkiStationProject.repositories.CourseRepository;
+import tn.esprit.SkiStationProject.services.CourseServicesImpl;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class CourseServicesImplMock {
+    @Mock
+    CourseRepository courseRepository = Mockito.mock(CourseRepository.class);
+
+    @InjectMocks
+    CourseServicesImpl courseService;
+
+    Course c = new Course(3, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 2f, 3, null);
+
+    List<Course> listCourses = new ArrayList<Course>() {
+        {
+            add(new Course(4, TypeCourse.COLLECTIVE_ADULT, Support.SNOWBOARD, 4f, 1, null));
+            add(new Course(9, TypeCourse.INDIVIDUAL, Support.SKI, 7f, 21, null));
+        }
+    };
+
+    @Test
+    public void testRetrieveCourse() {
+        Mockito.when(courseRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(c));
+        Course c1 = courseService.retrieveCourse((long) 1);
+        Assertions.assertNotNull(c1);
+    };
+
+    @Test
+    public void testRetrieveAllCourses() {
+        Mockito.when(courseRepository.findAll()).thenReturn(listCourses);
+        List<Course> lcs = courseService.retrieveAllCourses();
+        Assertions.assertNotEquals(0, lcs.size());
+    };
+}
---
+++ src/test/java/tn/esprit/Service/CourseServicesImplTest.java
@@ -0,0 +1,26 @@
+package tn.esprit.Service;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.SkiStationProject.SkiStationProjectApplication;
+import tn.esprit.SkiStationProject.entities.Course;
+import tn.esprit.SkiStationProject.services.ICourseServices;
+import java.util.List;
+
+@SpringBootTest(classes = {SkiStationProjectApplication.class})
+@TestMethodOrder(OrderAnnotation.class)
+public class CourseServicesImplTest {
+    @Autowired
+    ICourseServices cs;
+
+    @Test
+    @Order(1)
+    public void testRetrieveAllCourses() {
+        List<Course> courses = cs.retrieveAllCourses();
+        Assertions.assertEquals(0, courses.size());
+    }
+}
